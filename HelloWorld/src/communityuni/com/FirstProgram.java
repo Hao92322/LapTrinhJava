573a367b8847fdf2c37f92b3a1ac61df1f8bb295
Hao92322 <nguyenkhagiahao10@gmail.com>
2026-04-18 10:00:42 +0700

    Push source code chuong 1 Lap trinh Java chuong trinh HelloWorld

---
+++ HelloWorld/src/communityuni/com/FirstProgram.java
@@ -0,0 +1,27 @@
+package communityuni.com;
+
+public class FirstProgram {
+
+	public static void ptcl(double a,double b)
+	{
+		if(a==0 && b==0)
+			System.out.println("PT vo so nghiem");
+		else if (a==0 && b!=0)
+		{
+			System.out.println("PT vo nghiem");
+		}
+		else
+		{
+			System.out.println("Pt co nghiem x = "+(-b/a));
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ghi chu 1 dong
+		/*ghi chu nhieu dong
+		 * 22
+		 */
+		System.out.println("Xin chào các bạn");
+	}
+
+}
